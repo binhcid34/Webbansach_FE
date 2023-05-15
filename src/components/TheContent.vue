616abb9b0daa57d836e00cff084b86e5842630b4
pqbinh <binhcid34@gmail.com>
2023-05-15 21:53:45 +0700

    pqbinh

--- src/components/TheContent.vue
+++ src/components/TheContent.vue
@@ -311,6 +311,7 @@ export default {
         // this.getCategory();
         this.getAllProduct();
         this.toggleLoading(true);
+        this.getAllCategory();
     },
     mounted() {
         this.showSlides();
